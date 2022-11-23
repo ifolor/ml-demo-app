5f795add424696736e2634ea8306ba593f4f1d8f
Adele <adele@Adeles-MacBook-Pro-2.local>
2022-11-23 16:24:10 +0000

    created a feature for uploading an image

--- local/main.py
+++ local/main.py
@@ -5,23 +5,14 @@ from  PIL import Image
 
 
 #def main():
-st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
-with st.sidebar.expander("About the App"):
-     st.write("""
-        Use this simple app to convert your favorite photo to a pencil sketch, a grayscale image or an image with blurring effect.  \n  \nThis app was created by Sharone Li as a side project to learn Streamlit and computer vision. Hope you enjoy!
-     """)
+
+st.title("Image Processing App")
+#st.sidebar.title(" 👋 Hello ")
 
 
 
-#Create two columns with different width
-col1, col2 = st.columns( [0.5, 0.5])
-with col1:               # To display the header text using css style
-    st.markdown(""" <style> .font {
-    font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-    </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
 
-
+st.subheader("Upload your photo")
 uploaded_file = st.file_uploader("", type=['jpg','png','jpeg']) # To upload the photo
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -29,10 +20,21 @@ if uploaded_file is not None:
     col1, col2 = st.columns( [0.5, 0.5])
     with col1:
         st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
-        st.image(image,width=300)  
+        st.image(image,width=500)  
 
     with col2:
         st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+        filter = st.sidebar.radio('Covert your photo to:', ['Original','Gray Image','Black and White', 'Pencil Sketch', 'Blur Effect'])
+        if filter == 'Gray Image':
+                converted_img = np.array(image.convert('RGB'))
+                gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
+                st.image(gray_scale, width=300)
+        elif filter == 'Black and White':
+                converted_img = np.array(image.convert('RGB'))
+                gray_scale = cv2.cvtColor(converted_img, cv2.COLOR_RGB2GRAY)
+                slider = st.sidebar.slider('Adjust the intensity', 1, 255, 127, step=1)
+                (thresh, blackAndWhiteImage) = cv2.threshold(gray_scale, slider, 255, cv2.THRESH_BINARY)
+                st.image(blackAndWhiteImage, width=300)
 
 
 #if __name__ == "__main__":
