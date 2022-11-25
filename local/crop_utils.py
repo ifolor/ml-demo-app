e95e9c62fbf303a08ad1e886111d8f3d3cf5ef14
Adele <adele@Adeles-MacBook-Pro-2.local>
2022-11-25 16:48:36 +0000

    added utility cropping functions

--- local/crop_utils.py
+++ local/crop_utils.py
@@ -7,23 +7,20 @@ import numpy as np
 
 
 
-def create_threshold_image(input_image):
+def create_threshold_image(image):
     '''
     Creates a threshold image from the input image where the white regions
     correspond to the regions with high saliency values
     Parameters:
-        input_image (str): input image path
+        input_image (PIL.Image object): PIL.Image object from the uploaded file
     Returns:
         threshold_map (np.array): threshold image array with binary values
     '''
     # read the image as a greyscale image
-    image = Image.open(input_image)
     image_grey = ImageOps.grayscale(image)
     image_grey = np.array(image_grey)
     #image_grey = cv2.imread(input_image, cv2.IMREAD_GRAYSCALE)
-    
-    # save the greyscale image
-    #cv2.imwrite(os.path.join(output_dir, "".join([image_name, ".png"])), image_grey)
+
     # blur the image to reduce noise
     image_blurred = cv2.GaussianBlur(image_grey,(5,5),0)
     # create the saliency map
@@ -31,11 +28,8 @@ def create_threshold_image(input_image):
     (success, saliency_map) = saliency.computeSaliency(image_blurred)
     saliency_map = (saliency_map * 255).astype("uint8")
     # create the threshold map based on adaptive thresholding
-    threshold_value, threshold_map = cv2.threshold(saliency_map, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    
-    # save the threshold map
-    #cv2.imwrite(os.path.join(output_dir, "".join([image_name, "_threshold_map_v1.png"])), threshold_map)
-    return threshold_map
+    _, threshold_map = cv2.threshold(saliency_map, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    return saliency_map, threshold_map
 
 
 def ignore_small_contours(threshold_map, min_fraction=0.01):
@@ -55,8 +49,6 @@ def ignore_small_contours(threshold_map, min_fraction=0.01):
         if cv2.contourArea(contour) < min_fraction*threshold_map.shape[0]*threshold_map.shape[1]:
             # it seems like this function works only with the for loop:
             cv2.drawContours(threshold_map,[contour], 0, (0, 0, 0), -1)
-    #save the threshold map
-    cv2.imwrite(os.path.join(output_dir, "".join([image_name, "_threshold_map_v2.png"])), threshold_map)
     return threshold_map
 
 
--- local/main.py
+++ local/main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import numpy as np
 import cv2
-from  PIL import Image, ImageOps
-from create_crop_hints import create_threshold_image
+from  PIL import Image
+from crop_utils import create_threshold_image, ignore_small_contours
 st.set_page_config(layout="wide")
 
 
@@ -15,19 +15,28 @@ def main():
 
     uploaded_file = st.file_uploader("", type=['jpg','png','jpeg']) # To upload the photo
     if uploaded_file is not None:
+        print(uploaded_file)
         image = Image.open(uploaded_file)
-        threshold_map = create_threshold_image(uploaded_file)
+        saliency_map, threshold_map = create_threshold_image(image)
+        threshold_map_exclude_small_contours = ignore_small_contours(threshold_map)
 
     
-        col1, col2, col3, col4 = st.columns( [0.25, 0.25, 0.25, 0.25])
+        col1, col2, col3, col4, col5 = st.columns([0.2, 0.2, 0.2, 0.2, 0.2])
         with col1:
             st.markdown('<p style="text-align: center;">Original image</p>',unsafe_allow_html=True)
             st.image(image)  
 
         with col2:
-            st.markdown('<p style="text-align: center;">Threshold map</p>',unsafe_allow_html=True)
-            st.image(threshold_map)  
+            st.markdown('<p style="text-align: center;">Saliency map</p>',unsafe_allow_html=True)
+            st.image(saliency_map)  
 
+        with col3:
+            st.markdown('<p style="text-align: center;">Saliency map</p>',unsafe_allow_html=True)
+            st.image(threshold_map) 
+
+        with col4:
+            st.markdown('<p style="text-align: center;">Saliency map</p>',unsafe_allow_html=True)
+            st.image(threshold_map_exclude_small_contours)            
 
 
             #filter = st.sidebar.radio('Any desired aspect ratio (width-to-height)?', ['None','1:1','4:3', '4:5', '16:9'])
