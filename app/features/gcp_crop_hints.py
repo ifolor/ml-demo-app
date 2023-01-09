81e5a034fa2294947cb78bf8291827be89fc09fa
adele.kuzmiakova <adele02@pm.me>
2023-01-09 17:08:16 +0100

    updated gcp crop hints

--- app/features/gcp_crop_hints.py
+++ app/features/gcp_crop_hints.py
@@ -1,11 +1,25 @@
 import os
+import numpy as np
+from PIL import Image
+import cv2
 from google.cloud import vision  
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "369410-4d30b4b7a194.json"
 
 client = vision.ImageAnnotatorClient()
 
 def create_bounding_box(image_bytes, aspect_ratio=None):
-
+    '''
+    Utilizes GCP crop hints function to return the bounding box coordinates for a given image
+    Parameters:
+        image_bytes (bytes): image that was uploaded using the Streamlit app
+        aspect_ratio (float): width/height of the desired bounding box, e.g. 16/9, optional
+    Returns:
+        vertices (list): list of 4 strings containing the x- and y-coordinates of the bounding 
+            box, e.g. ['(0,1161)', '(3023,1161)', '(3023,2870)', '(0,2870)']
+        hint.confidence (float): the confidence level of the detected bounding box
+        hint.importance_fraction (float): the importance level of the detected bounding 
+        box
+    '''
     image = vision.Image(content=image_bytes)
     if aspect_ratio is None:
         response = client.crop_hints(image=image)
@@ -22,9 +36,57 @@ def create_bounding_box(image_bytes, aspect_ratio=None):
 
 
 def calculate_bounding_box_coords(vertices):
-
+    '''
+    Parses the vertices response from the GCP crop_hints function to extract the upper left corner
+        of the bounding box with its width and height
+    Parameters:
+        vertices (list): list of 4 strings containing the x- and y-coordinates of the bounding 
+            box, e.g. ['(0,1161)', '(3023,1161)', '(3023,2870)', '(0,2870)']
+    Returns:
+        start_x (int): the x-coordinate of the upper left corner of the bounding box
+        start_y (int): the y-coordinate of the upper left corner of the bounding box
+        width (int): width of the bounding box
+        height (int): height of the bounding box
+    '''
     start_x = int(vertices[0].split(",")[0].replace('(', ''))
     start_y = int(vertices[0].split(",")[1].replace(')', ''))
     width = int(vertices[1].split(",")[0].replace('(', '')) - start_x
     height = int(vertices[2].split(",")[1].replace(')', '')) - start_y
     return start_x, start_y, width, height
+
+
+def create_cropped_image(image, start_x, start_y, width, height):
+    '''
+    Crops the image based on the bounding rectangle
+    Parameters:
+        image (PIL.Image class)
+        start_x (int): the x-coordinate of the upper left corner of the bounding box
+        start_y (int): the y-coordinate of the upper left corner of the bounding box
+        width (int): width of the bounding box
+        height (int): height of the bounding box
+    Returns:
+        cropped_image (array): image where only the area within the bounding box is preserved
+    '''
+    image_array = np.asarray(image)
+    cropped_image = image_array[start_y:start_y+height,  start_x:start_x+width]
+    return cropped_image
+
+def create_rectangle_image(image, start_x, start_y, width, height, colour = (238, 40, 103)):
+    '''
+    Draws the bounding box on the original image
+    Parameters:
+        image (PIL.Image class)
+        start_x (int): the x-coordinate of the upper left corner of the bounding box
+        start_y (int): the y-coordinate of the upper left corner of the bounding box
+        width (int): width of the bounding box
+        height (int): height of the bounding box
+        color (tuple): RGB color representation, optional
+    Returns:
+        rectangle_image (array): image containing the bounding box
+    '''
+    image_array = np.asarray(image)
+    start_vertex = (start_x, start_y)
+    end_vertex = (start_x+width, start_y+height)
+    thickness = int(0.02*min(image_array.shape[0], image_array.shape[1]))
+    rectangle_image = cv2.rectangle(image_array, start_vertex, end_vertex, colour, thickness)
+    return rectangle_image
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import cv2
 from PIL import Image
 from google.cloud import vision
 from crop_utils import create_threshold_image, ignore_small_contours, crop_image
-from features.gcp_crop_hints import create_bounding_box, calculate_bounding_box_coords
+from features.gcp_crop_hints import *
 
 st.set_page_config(layout="wide")
 st.write('<style>div.block-container{padding-top:-20rem;}</style>', unsafe_allow_html=True)
@@ -19,30 +19,17 @@ st.subheader("Please select which feature you want to test.")
 
 uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
 if uploaded_file is not None:
-    print(type(uploaded_file))
     image_bytes = uploaded_file.read()
+    image = Image.open(uploaded_file)
     vertices, confidence, importance_fraction = create_bounding_box(image_bytes, aspect_ratio=16/9)
     start_x, start_y, width, height = calculate_bounding_box_coords(vertices)
+    cropped_image = create_cropped_image(image, start_x, start_y, width, height)
+    rectangle_image = create_rectangle_image(image, start_x, start_y, width, height)
 
-
-
-    image = Image.open(uploaded_file)
-    image_array = np.asarray(image)
-        # crop the image based on the bounding rectangle
-    cropped_image = image_array[start_y:start_y+height,  start_x:start_x+width]
-        # draw the rectangle on the original image
-    start_vertex = (start_x, start_y)
-    end_vertex = (start_x+width, start_y+height)
-    colour = (238, 40, 103)
-    thickness = int(0.02*min(image_array.shape[0], image_array.shape[1]))
-    rectangle_image = cv2.rectangle(image_array, start_vertex, end_vertex, colour, thickness)
 
     col_1, col_2, col_3, col_4, col_5 = st.columns(np.ones(5)*0.2)
     with col_1:
      
-       
-        
-
  
         st.markdown('<p style="text-align: center;">Original image</p>',unsafe_allow_html=True)
         st.image(image, channels="BGR") 
